8d7d6abe2ce19b3128fc99a2128e0fc168c97190
Jewelz <hujewelz@163.com>
2020-08-31 19:48:11 +0800

    Login with Google

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Sidebar from "./Components/Sidebar";
 import Chat from "./Components/Chat";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./Components/Login";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{ user }, dispatch] = useStateValue();
+
   return (
     <div className="App">
-      <div className="App-body">
-        <Router>
-          <Sidebar />
-          <Switch>
-            <Route path="/rooms/:roomId" component={Chat}></Route>
-            <Route path="/" component={Chat}></Route>
-          </Switch>
-        </Router>
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="App-body">
+          <Router>
+            <Sidebar />
+            <Switch>
+              <Route path="/rooms/:roomId" component={Chat}></Route>
+              <Route path="/" component={Chat}></Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
