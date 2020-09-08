4ebb85f038b9307a509ad88aadb474baef589ff7
huluobo <hujewelz@163.com>
2020-09-08 14:43:08 +0800

    Refactor Post

--- instagram-clone/src/screen/Post.js
+++ instagram-clone/src/screen/Post.js
@@ -4,16 +4,15 @@ import PostItem from "../components/Post";
 
 function Post() {
   return (
-    <div className="post">
-      <div className="post-content">
-        <div className="post-container">
-          <PostItem />
-          <PostItem />
-          <PostItem />
-        </div>
-        <div className="sidebar-right">
-          <h1>Suggestion For You</h1>
-        </div>
+    <div className="main">
+      <div className="post-container">
+        <PostItem />
+        <PostItem />
+        <PostItem />
+      </div>
+
+      <div className="sidebar-right">
+        <h1>Suggestion For You</h1>
       </div>
     </div>
   );
